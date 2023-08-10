13cf1f57c4f2b8b77db1e913eddee88ca8136c9a
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-08-10 22:04:07 +0300

    fixing bugs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import FeedbackOptions from './feedback/FeedbackOptions';
 import Statistic from './feedback/Statistic';
+import Notification from './Notification';
 import '../components/styles.css';
 
 export class App extends React.Component {
@@ -15,12 +16,21 @@ export class App extends React.Component {
       [type]: prevState[type] + 1,
     }));
   };
-
+  countTotal = () => {
+    return Object.values(this.state).reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+  };
+  countPositiveFeedback = () => {
+    const total = this.countTotal();
+    return total === 0 ? 0 : (this.state.good / total) * 100;
+  };
   render() {
     const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    const positiveFeedback = total === 0 ? 0 : (good / total) * 100;
-
+    const total = this.countTotal();
+    const positiveFeedback = this.countPositiveFeedback();
+    const hasFeedback = total > 0;
     return (
       <div className="container">
         <h1>Please leave feedback</h1>
@@ -28,13 +38,18 @@ export class App extends React.Component {
           options={['good', 'neutral', 'bad']}
           onLeaveFeedback={this.handleFeedback}
         />
-        <Statistic
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positiveFeedback={positiveFeedback.toFixed(0)}
-        />
+        <h2>Statistic</h2>
+        {hasFeedback ? (
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positiveFeedback={positiveFeedback.toFixed(0)}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
       </div>
     );
   }
--- src/components/feedback/Statistic.jsx
+++ src/components/feedback/Statistic.jsx
@@ -1,19 +1,11 @@
 const Statistic = ({ good, neutral, bad, total, positiveFeedback }) => {
-  const hasFeedback = good > 0 || neutral > 0 || bad > 0;
   return (
     <div>
-      <h2>Statistic</h2>
-      {hasFeedback ? (
-        <div>
-          <p>Good: {good}</p>
-          <p>Neutral: {neutral}</p>
-          <p>Bad: {bad}</p>
-          <p>Total: {total}</p>
-          <p>PositiveFeedback: {positiveFeedback}%</p>
-        </div>
-      ) : (
-        <p>"There is no feedback"</p>
-      )}
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>PositiveFeedback: {positiveFeedback}%</p>
     </div>
   );
 };
